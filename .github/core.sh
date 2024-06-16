d2bb56668670c7aef6bb126a674ffa8afd04903e
White12352 <86940955+White12352@users.noreply.github.com>
2024-06-16 15:16:34 +0800

    Update core.sh

--- .github/core.sh
+++ .github/core.sh
@@ -4,8 +4,8 @@ buildScript/lib/core/init.sh
 cd ..
 rm -rf sing-box
 rm -rf sing
-git clone -b sing-box-b-t https://github.com/White12352/sing-box.git sing-box
-git clone -b dev https://github.com/White12352/sing.git sing
+git clone -b sing-box-b-t-h https://github.com/White12352/sing-box.git sing-box
+git clone -b dev-tl https://github.com/White12352/sing.git sing
 cd sjnh/libcore
 go mod tidy
 cd ..
